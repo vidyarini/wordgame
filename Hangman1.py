0d8c9a93fa0f1a6a3bd8b32926593a50e8d707e5
vidyarini <vidyarini@gmail.com>
2020-09-12 19:10:56 -0400

    added increased difficulty feature

--- Hangman1.py
+++ Hangman1.py
@@ -6,7 +6,7 @@ curr = conn.cursor()
 
 class Hangman:
 
-    def __init__(self, word):
+    def __init__(self, word, streak):
         self.word = word.lower()
         self.blank = ""
         self.count = 10
@@ -15,10 +15,12 @@ class Hangman:
         self.hint = ''
         self.blank_array = []
         self.score_list = []
+        self.streak = streak
 
 
-    # Check if the given string is valid
+    # Check if the given string is valid for multipalyer mode when the word is given by an opponent
     def string_validity(self):
+
         while len(self.word) < 6 or self.word.isalpha() == False:
             if len(self.word) < 6:
                 self.word = input("Enter a word with at least 6 character: ")
@@ -29,10 +31,11 @@ class Hangman:
         print(self.blank)
         return
 
+
     # Accept a single character from the user and check its validity
     def character_validity(self, letter):
 
-        while letter.isalpha() == False or len(letter) != 1 :
+        while letter.isalpha() == False or len(letter) != 1:
             if letter.lower() == "exit":
                 return 'Exit'
             letter = input("Please enter a single alphabet: ")
@@ -44,6 +47,7 @@ class Hangman:
             self.letters_entered.append(letter)
 
         return letter.lower()
+
 
     #Check for the presence of the letter in the string
     def letter_presence(self):
@@ -58,9 +62,10 @@ class Hangman:
                     return
                 self.play(valid_letter, self.count)
             else:
+                self.streak += 1
                 print("Congratulations you won!")
                 print("Your score for this game is:", self.score())
-                return self.score()
+                return [self.score(), self.streak]
                 break
 
             if (self.count == 4 and self.blank.count('_') > 1)  and self.status == True:
@@ -68,13 +73,14 @@ class Hangman:
                 self.status = False
 
         if self.count == 0:
-
+            self.streak = 0
             print("The word was:", self.word)
             print("Your score for this game is:", self.score())
             print("Better luck next time.")
+            return [self.score(), self.streak]
 
 
-    #Play each letter as the player gusses
+    #Play each letter as the player guesses
     def play(self,valid_letter,count):
         if valid_letter in self.word:
             indices = []
@@ -94,6 +100,7 @@ class Hangman:
             print(self.blank)
 
 
+    #Option to get a clue
     def need_clue(self):
         self.hint = input("Do you need a clue?(Y/N): ")
 
@@ -129,25 +136,44 @@ class Hangman:
 
 query = "Select * from Easy"
 curr.execute(query)
-results = list(curr.fetchall())
+results1 = list(curr.fetchall())
+
+query2 = "Select * from Hard"
+curr.execute(query2)
+results2 = list(curr.fetchall())
 
 word_selected = ""
 score_list = []
 Want_to_Play = 'Y'
 words_used = []
+streak = 0
 
 while(Want_to_Play.upper() == 'Y'):
+
+    if streak < 2: results = results1
+    else: results = results2
 
     while len(word_selected) < 6 or (word_selected in words_used) or word_selected.find('-') != -1:
          word_selected = choice(results)[1]
     words_used.append(word_selected)
 
-    test = Hangman(word_selected)
+    test = Hangman(word_selected, streak)
     test.string_validity()
 
-    score_list.append(test.letter_presence())
+    [score_l, streak] = test.letter_presence()
+    score_list.append(score_l)
     print("Total Score:", sum(score_list))
     Want_to_Play = input("Do you want to play again?(Y/N): ")
+
+
+    if streak < 5: results = results1
+    else: results = results2
+
+
+
+
+
+
 
 
 
--- Hangman_ETL.py
+++ Hangman_ETL.py
@@ -1,21 +1,35 @@
 import pymysql
 from random import randint, choice
 
-File_object = open(r"C:\Users\vidya\Desktop\Medium_words.txt", "r")
-
-File = File_object.read()
-Word_array = File.split("\n")
-
-
-
 # conncet to DB
 conn = pymysql.connect("127.0.0.1", user="root", passwd="password", db="Hangman", port=3306)
 curr = conn.cursor()
 
+#Easy words
+File_object = open(r"C:\Users\vidya\Desktop\Easy_words.txt", "r")
+File = File_object.read()
+Word_array = File.split("\n")
+
 for word in Word_array:
      query = "Insert into Easy(word) values('{}')".format(word)
-     curr.execute(query)#
+     curr.execute(query)
+
+
+#Hard_words
+File_object = open(r"C:\Users\vidya\Desktop\Hard_words.txt", "r")
+File = File_object.read()
+Word_array = File.split("\n")
+
+for word in Word_array:
+     query = "Insert into Hard(word) values('{}')".format(word)
+     curr.execute(query)
+
 conn.commit()
+
+
+
+
+
 
 
 
